68492204be5771ee1eb0dbf1e44bd8f4649ed8f1
Ilari Mäkelä <ilari.makela@solinor.com>
2014-06-07 21:53:10 +0300

    Initial commit to Github

---
+++ install.sh
@@ -0,0 +1,28 @@
+# https://github.com/jamiew/git-friendly
+# the `push` command which copies the github compare URL to my clipboard is heaven
+sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+
+# https://rvm.io
+# rvm for the rubiess
+curl -L https://get.rvm.io | bash -s stable --ruby
+
+# https://github.com/isaacs/nave
+# needs npm, obviously.
+# TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
+npm install -g nave
+
+# homebrew!
+# you need the code CLI tools YOU FOOL.
+ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+
+# https://github.com/rupa/z
+# z, oh how i love you
+mkdir -p ~/util/z
+curl https://raw.github.com/rupa/z/master/z.sh > ~/util/z/z.sh
+chmod +x ~/util/z/z.sh
+
+cd ~/util
+git clone git://github.com/dronir/SpotifyControl.git
+
+cd "$(dirname "$0")"
+./sync.sh
