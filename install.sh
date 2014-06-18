2fca845518b969518c6ffabbbd96c94a3a44f4e5
Ilari Mäkelä <ilari@Ilaris-MacBook-Pro.local>
2014-06-18 09:13:26 +0300

    Useless comments removed, brew install fixed and installation of node added

--- install.sh
+++ install.sh
@@ -6,17 +6,16 @@ sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh
 # rvm for the rubiess
 curl -L https://get.rvm.io | bash -s stable --ruby
 
+# homebrew
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew doctor
+
+brew install node
+
 # https://github.com/isaacs/nave
-# needs npm, obviously.
-# TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
 npm install -g nave
 
-# homebrew!
-# you need the code CLI tools YOU FOOL.
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
-
 # https://github.com/rupa/z
-# z, oh how i love you
 mkdir -p ~/util/z
 curl https://raw.github.com/rupa/z/master/z.sh > ~/util/z/z.sh
 chmod +x ~/util/z/z.sh
