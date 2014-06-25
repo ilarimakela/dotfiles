8e4c7f18fad52b48da8f2f323dc41dc455a4b41b
Ilari Mäkelä <ilari.makela@solinor.com>
2014-06-25 15:04:50 +0300

    Promt removed from sync

--- sync.sh
+++ sync.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
-fi
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
 
 source ~/.bash_profile
